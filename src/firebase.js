1275cca7251be63e79049ad9c0c2cdb55dc33918
muath alzoubi <32450805+muathzoubi@users.noreply.github.com>
2023-10-12 03:21:07 +0000

    Update 2 files

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAFqsglDDkezsxu6AsIrQkNcaI4alElSxc",
-  authDomain: "first-97cb5.firebaseapp.com",
-  databaseURL: "https://first-97cb5-default-rtdb.firebaseio.com",
-  projectId: "first-97cb5",
-  storageBucket: "first-97cb5.appspot.com",
-  messagingSenderId: "737995060180",
-  appId: "1:737995060180:web:9942daf72da7d06a965c41",
-  measurementId: "G-KJ9RJ6W707"
+  apiKey: "AIzaSyAauhQIclhvlQW6fkuLpr-9ltP-bBnOjwM",
+  authDomain: "chatly-5c30e.firebaseapp.com",
+  databaseURL: "https://chatly-5c30e-default-rtdb.firebaseio.com",
+  projectId: "chatly-5c30e",
+  storageBucket: "chatly-5c30e.appspot.com",
+  messagingSenderId: "1008977128346",
+  appId: "1:1008977128346:web:1030116f13c8256d746823",
+  measurementId: "G-BTVL1YQGMR",
 };
 
 firebase.initializeApp(firebaseConfig);
